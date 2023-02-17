334b8b3738817b218c9faf8b43e38586076b7f71
gonnavis <gonnavis@gmail.com>
2023-02-18 01:28:59 +0800

    ok: Torus land with water.

--- index.js
+++ index.js
@@ -4,23 +4,51 @@ import * as THREE from 'three';
 const localVector2D = new THREE.Vector2();
 
 export default (ctx) => {
-  const { useApp, useFrame, useLocalPlayer } = ctx;
+  const { useApp, useFrame, useLocalPlayer, usePhysics, useCleanup } = ctx;
   const app = useApp();
   const localPlayer = useLocalPlayer();
+  const physics = usePhysics();
+
+  const geometry = new THREE.TorusGeometry( 7, 3, 6, 100 );
+  geometry.rotateX(Math.PI / 2);
+  geometry.scale(3, 1, 3);
+  const material = new THREE.MeshStandardMaterial( { color: new THREE.Color(`rgb(164, 93, 37)`), flatShading: true } );
+  const torus = new THREE.Mesh( geometry, material );
+  torus.position.y = -3;
+  app.add( torus );
+
+  const physicsObject = physics.addGeometry(torus);
+
+  {
+    const geometry = new THREE.PlaneGeometry( 1000, 1000 );
+    geometry.rotateX(Math.PI / 2);
+    const material = new THREE.MeshStandardMaterial( {color: 'cyan', opacity: 0.5, transparent: true, side: THREE.DoubleSide} );
+    const water = new THREE.Mesh( geometry, material );
+    water.position.y = -1.3;
+    app.add( water );
+  }
 
   useFrame(({ timestamp }) => {
-    localVector2D.x = localPlayer.position.x;
-    localVector2D.y = localPlayer.position.z;
-    if (localVector2D.length() > 3) {
+    // localVector2D.x = localPlayer.position.x;
+    // localVector2D.y = localPlayer.position.z;
+    // if (localVector2D.length() > 3) {
+    if (localPlayer.position.y <= -1) {
       if (!localPlayer.actionManager.hasActionType('swim')) {
         const newSwimAction = {type: 'swim'};
         localPlayer.actionManager.addAction(newSwimAction);
+        localPlayer.actionManager.hasActionType('fallLoop') && localPlayer.actionManager.removeActionType('fallLoop');
+        localPlayer.actionManager.hasActionType('skydive') && localPlayer.actionManager.removeActionType('skydive');
+        localPlayer.actionManager.hasActionType('glider') && localPlayer.actionManager.removeActionType('glider');
       }
     } else {
       if (localPlayer.actionManager.hasActionType('swim')) {
         localPlayer.actionManager.removeActionType('swim');
       }
     }
+  });
+  
+  useCleanup(() => {
+    physics.removeGeometry(physicsObject);
   });
 
   app.setComponent('renderPriority', 'low');
